650b4651f9c4bdd44c5eec35aa05067a5c0d6a04
tsmahur <32067811+tsmahur@users.noreply.github.com>
2021-12-10 00:46:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2! BY me')
+  res.send('CICD App V2! BY me - finalyyyy')
 })
 
 app.get('/status', (req, res) => {
