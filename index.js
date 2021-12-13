0f09e7dd7f773b6bd900384c7c232501882e9bd1
tsmahur <32067811+tsmahur@users.noreply.github.com>
2021-12-13 13:45:53 +0530

    webhook push 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require('config')
 console.log(config);
 
 app.get('/', (req, res) => {
-  res.send('CICD App V2! BY me - finalyyyy - git web hook-2')
+  res.send('CICD App V2! BY me - finalyyyy - git web hook-2 v1')
 })
 
 app.get('/status', (req, res) => {
